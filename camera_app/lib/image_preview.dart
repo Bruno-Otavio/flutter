d8ddf7625d11add761e2835f90a539f9cead2ab2
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-07-25 15:46:55 -0300

    save file function (not working)

--- camera_app/lib/image_preview.dart
+++ camera_app/lib/image_preview.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
 
 class ImagePreview extends StatefulWidget {
   final XFile file;
@@ -13,6 +15,12 @@ class ImagePreview extends StatefulWidget {
 }
 
 class _ImagePreviewState extends State<ImagePreview> {
+  Future<void> _savePicture(XFile picture) async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    File file = File(path.join(directory.path, path.basename(picture.path)));
+    await picture.saveTo(directory.path);
+  }
+
   @override
   Widget build(BuildContext context) {
     File picture = File(widget.file.path);
@@ -21,8 +29,25 @@ class _ImagePreviewState extends State<ImagePreview> {
       appBar: AppBar(
         title: const Text('Image Preview'),
       ),
-      body: Center(
-        child: Image.file(picture),
+      body: Column(
+        children: [
+          Image.file(picture),
+          Padding(
+            padding: const EdgeInsets.only(top: 25),
+            child: ElevatedButton(
+              onPressed: () => _savePicture(widget.file),
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Save Image',
+                  style: TextStyle(
+                    fontSize: 25
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
