6680c0c7a2e0719a4d6b7ef0a3018f1964fb7c31
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-07-26 15:15:08 -0300

    refactor

--- camera_app/lib/home.dart
+++ camera_app/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:camera/camera.dart';
+import 'package:camera_app/image_list.dart';
 import 'package:camera_app/image_preview.dart';
 import 'package:flutter/material.dart';
 
@@ -9,18 +10,19 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
   List<CameraDescription> _cameras = [];
   CameraController? _cameraController;
+  bool isFrontCamera = false;
 
-  Future<void> _setupCameraController() async {
+  Future<void> _setupCameraController(int camera) async {
     List<CameraDescription> cameras = await availableCameras();
 
     if (cameras.isNotEmpty) {
       setState(() {
         _cameras = cameras;
         _cameraController =
-            CameraController(cameras.first, ResolutionPreset.max);
+            CameraController(_cameras[camera], ResolutionPreset.max);
       });
 
       _cameraController?.initialize().then((_) {
@@ -28,6 +30,8 @@ class _HomePageState extends State<HomePage> {
           return;
         }
         setState(() {});
+      }).catchError((Object e) {
+        debugPrint('$e');
       });
     }
   }
@@ -54,17 +58,80 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    if (_cameraController == null || !_cameraController!.value.isInitialized) {
+      return;
+    }
+
+    if (state == AppLifecycleState.inactive) {
+      _cameraController?.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      _setupCameraController(0);
+    }
+  }
+
+  @override
   void initState() {
     super.initState();
-    _setupCameraController();
+    _setupCameraController(0);
   }
 
   @override
   Widget build(BuildContext context) {
-    return _buildUI();
+    return SafeArea(
+      child: SizedBox.expand(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _showCamera(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const ImageList(),
+                        ),
+                      );
+                    },
+                    iconSize: 50,
+                    icon: const Icon(Icons.image),
+                  ),
+                  IconButton(
+                    onPressed: _takePicture,
+                    iconSize: 75,
+                    icon: const Icon(Icons.camera),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isFrontCamera = !isFrontCamera;
+                      });
+              
+                      // Change between frontal and back camera
+                      isFrontCamera
+                          ? _setupCameraController(1)
+                          : _setupCameraController(0);
+                    },
+                    iconSize: 50,
+                    icon: const Icon(Icons.flip_camera_android),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
-  Widget _buildUI() {
+  Widget _showCamera() {
     if (_cameraController == null ||
         _cameraController?.value.isInitialized == false) {
       return const Center(
@@ -72,40 +139,10 @@ class _HomePageState extends State<HomePage> {
       );
     }
 
-    return SafeArea(
-      child: SizedBox.expand(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: MediaQuery.sizeOf(context).height * 0.70,
-              child: CameraPreview(
-                _cameraController!,
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  iconSize: 50,
-                  icon: const Icon(Icons.image),
-                ),
-                IconButton(
-                  onPressed: _takePicture,
-                  iconSize: 75,
-                  icon: const Icon(Icons.camera),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  iconSize: 50,
-                  icon: const Icon(Icons.flip_camera_android),
-                ),
-              ],
-            )
-          ],
-        ),
+    return SizedBox(
+      height: MediaQuery.sizeOf(context).height * 0.70,
+      child: CameraPreview(
+        _cameraController!,
       ),
     );
   }
