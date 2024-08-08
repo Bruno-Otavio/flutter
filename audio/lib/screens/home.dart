44f9c2efe980bf3c0bb8898bd728b60e8db94f08
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-08-08 15:50:37 -0300

    audio player

---
+++ audio/lib/screens/home.dart
@@ -0,0 +1,46 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+import 'package:record/record.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final AudioPlayer _audioPlayer = AudioPlayer();
+
+  final AudioRecorder _audioRecorder = AudioRecorder();
+  bool _isRecording = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsFlutterBinding.ensureInitialized();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Audio Player'),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: ElevatedButton(
+            onPressed: () async {
+              _audioPlayer.play(AssetSource('metal_pipe_falling.mp3'));
+            },
+            child: const Text('Play Audio'),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: !_isRecording ? const Icon(Icons.mic) : const Icon(Icons.pause),
+      ),
+    );
+  }
+}
